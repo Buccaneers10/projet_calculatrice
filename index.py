9db8cfef01601820e56bcbb12cbbe7ca8f9eac42
Buccaneers10 <boucanier.pro@protonmail.com>
2022-09-07 21:36:03 +0200

    Ajout de la correction

--- index.py
+++ index.py
@@ -4,3 +4,8 @@ nombre1 = int(nombre1)
 nombre2 = int(nombre2)
 resultat = f"Le résultat de l'addition du nombre {nombre1} avec le nombre {nombre2} est égal à {nombre1 + nombre2}"
 print(resultat)
+
+# Correction:
+# a = input("Entrez un premier nombre: ")
+# b = input("Entrez un deuxième nombre: ")
+# print(f"Le résultat de l'addition de {a} avec {b} est égal {int(a) + int(b)}")
