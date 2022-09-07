6df56c0eada36f3f5b973619b713e0a82b21c08e
Buccaneers10 <boucanier.pro@protonmail.com>
2022-09-07 21:27:04 +0200

    Réalisation du script

---
+++ index.py
@@ -0,0 +1,9 @@
+import re
+
+
+nombre1 = input("Veuillez saisir le premier nombre: ")
+nombre2 = input("Veuillez saisir le deuxième nombre: ")
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+resultat = f"Le résultat de l'addition du nombre {nombre1} avec le nombre {nombre2} est égal à {nombre1 + nombre2}"
+print(resultat)
